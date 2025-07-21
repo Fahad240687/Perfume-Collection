61302d0f9399d27ae5a357adf731271c7166c5f8
Fahad240687 <fahad.techeyedigital@gmail.com>
2025-07-21 17:32:43 +0500

    dad

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    domains: ['localhost'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '3000',
-      }
-    ],
-    unoptimized: true,
-  },
-}
+const nextConfig = {};
 
-export default nextConfig
+export default nextConfig;
